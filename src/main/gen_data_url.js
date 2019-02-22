ae555467a6659f8c06eb4eddd893ca69350f8c0e
Sutton <suttonpage2000@gmail.com>
2019-02-22 17:30:14 +0300

    Converted covertsong variable to function in gen_data_url.js

--- src/main/gen_data_url.js
+++ src/main/gen_data_url.js
@@ -1,20 +1,22 @@
 import dataurl from 'dataurl';
 
 
-module.exports = {
-  convertSong :(filePath) => {
+
+
+function convertSong(filePath){
     const songPromise = new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) { reject(err); }
-        resolve(dataurl.convert({ data, mimetype: 'audio/mp3' }));
+        fs.readFile(filePath, (err, data) => {
+          if (err) { reject(err); }
+          resolve(dataurl.convert({ data, mimetype: 'audio/mp3' }));
+        });
       });
-    });
-    return songPromise;
-  }
+      return songPromise;
+    }
 
+
+module.exports = {
+    convertSong
 };
-
-
 
 
 
