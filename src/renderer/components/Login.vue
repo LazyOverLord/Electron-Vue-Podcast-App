b3527888f980193319c2b904693b208b69e0bec9
Sutton <suttonpage2000@gmail.com>
2019-03-03 21:04:43 +0300

    Added firebase test login code

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -1,9 +1,20 @@
 <template>
     <div>
-        <p> Enter your username  <input v-model="user_name"> </p>
+        <p> Sign Up </p>
+        <p> Enter your email <input v-model="user_name"> </p>
         <p> Enter your password <input v-model="password"> </p>
         <button @click="add_user()"> Add User </button>
-        <p id="error_message"> error:  {{err_message}}</p>
+        <p class="error_message"> error:  {{err_message}}</p>
+
+
+        <p> Sign In </p>
+            <p> Enter your email <input v-model="user_name_login"> </p>
+            <p> Enter your password <input v-model="password_login"> </p>
+            <button @click="login_in_user()"> Login In </button>
+            <p class="error_message"> error:  {{login_error}}</p>
+
+
+        
     </div>
 
 </template>
@@ -15,24 +26,49 @@ export default {
         return {
             user_name:"",
             password:"",
-            err_message:""
+            err_message:"",
+            user_name_login:"",
+            password_login:"",
+            login_error:""
         }
     },
 
     methods:{
         add_user:function(){
-            this.$auth.createUserWithEmailAndPassword(this.user_name,this.password).catch((err)=>{
+            this.$auth.createUserWithEmailAndPassword(this.user_name,this.password)
+            .then((result)=>{
+                console.log(result);
+            })
+        
+            
+            .catch((err)=>{
                 var err_code = err["code"];
                 console.log(err_code);
                 this.err_message = err_code;
+                if(err_code == undefined){
+                    this.user_name = "";
+                    this.password = "";
+                }
+            })
+        },
+
+        login_in_user:function(){
+            this.$auth.signInWithEmailAndPassword(this.user_name_login,this.password_login).then((result)=>{
+                console.log(result);
+            })
+
+            .catch((err)=>{
+                console.log(err);
             })
         }
+
+
     }
 }
 </script>
 
 <style scoped>
-    #error_message {
+    .error_message {
         color:red;
     }
 </style>
