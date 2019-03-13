5900de08722394fd286c4b435cb550c763cd7305
Sutton <suttonpage2000@gmail.com>
2019-03-13 21:26:25 +0300

    Revert "small fix"
    This reverts commit 612f06c7db002e2b11ffbdee302dfe7badc61e8f.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,7 +6,14 @@ import store from './store'
 import firebase from 'firebase';
 import * as firebaseui from 'firebaseui'
 
-
+var config = {
+  apiKey: "***REMOVED***",
+  authDomain: "***REMOVED***",
+  databaseURL: "***REMOVED***",
+  projectId: "***REMOVED***",
+  storageBucket: "***REMOVED***.appspot.com",
+  messagingSenderId: "***REMOVED***"
+};
 
 
 
