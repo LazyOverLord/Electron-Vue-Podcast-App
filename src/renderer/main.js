612f06c7db002e2b11ffbdee302dfe7badc61e8f
Sutton <suttonpage2000@gmail.com>
2019-03-13 21:19:32 +0300

    Removed firebase creds

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,14 +6,7 @@ import store from './store'
 import firebase from 'firebase';
 import * as firebaseui from 'firebaseui'
 
-var config = {
-  apiKey: "***REMOVED***",
-  authDomain: "***REMOVED***",
-  databaseURL: "***REMOVED***",
-  projectId: "***REMOVED***",
-  storageBucket: "***REMOVED***.appspot.com",
-  messagingSenderId: "***REMOVED***"
-};
+
 
 
 
