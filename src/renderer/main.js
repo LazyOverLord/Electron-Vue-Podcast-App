9373e661ad5a81cb2c25d9922b63fa5e00daeebb
Sutton <suttonpage2000@gmail.com>
2019-03-14 19:39:45 +0300

    Removed the firebase code from main.js in render folder

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,24 +3,19 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
-import firebase from 'firebase';
-import * as firebaseui from 'firebaseui'
+
 //added a comment
 
 
 
-var firebase_obj =firebase.initializeApp(config);
 
-//var auth_ui = new firebaseui.auth.AuthUI(firebase_obj.auth());
 
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-Vue.prototype.$auth = firebase_obj.auth();
-//Vue.prototype.$authUi = auth_ui;
-Vue.prototype.$database = firebase_obj.database();
+
 
 
 
