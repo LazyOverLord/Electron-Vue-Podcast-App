fbc5d94b452f9181c8203792c170bbcd822d9e36
Sutton <suttonpage2000@gmail.com>
2019-03-17 15:41:06 +0300

    Added card test component to project

---
+++ src/renderer/components/Card-Test.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+
+    <router-link to="/"> Go Home </router-link>
+    <md-card>
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">Media card</div>
+          <div class="md-subhead">Big size</div>
+        </md-card-header-text>
+
+        <md-card-media md-big>
+          <img src="https://vuematerial.io/assets/examples/cover.png" alt="People">
+        </md-card-media>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button>Action</md-button>
+        <md-button>Action</md-button>
+      </md-card-actions>
+    </md-card>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"card_test",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
