bd3402d8c29133964f7476e08c27ecbf882e1c1b
Sutton <suttonpage2000@gmail.com>
2019-03-18 18:19:48 +0300

    Changed class on md-toolbar in nav-test to transparent

--- src/renderer/components/Nav-Test.vue
+++ src/renderer/components/Nav-Test.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-container md-layout-column">
-    <md-toolbar class="md-primary">
+    <md-toolbar class="md-transparent">
       <md-button class="md-icon-button" @click="showNavigation = true">
         <md-icon>menu</md-icon>
       </md-button>
