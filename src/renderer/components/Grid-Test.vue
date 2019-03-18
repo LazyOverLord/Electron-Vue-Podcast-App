65e4921d682c3208b1bef9d4a69bc720424cf738
Sutton <suttonpage2000@gmail.com>
2019-03-18 20:59:51 +0300

    Removed progress spinner from Podcast-Episodes component

--- src/renderer/components/Grid-Test.vue
+++ src/renderer/components/Grid-Test.vue
@@ -1,10 +1,58 @@
 <template>
-    <div class="md-layout">
-        <div class="md-layout-item md-large">
-            <router-link to="/"> Go Home </router-link>
+    <div class="md-layout md-gutter md-alignment-center">
+        <div class="md-layout-item ">
+            <md-card>
+      <md-card-header>
+        <div class="md-title">Card without hover effect</div>
+      </md-card-header>
+
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+      </md-card-content>
+
+      <md-card-actions>
+        <router-link to="/"> 
+            <md-button> Go Home </md-button>
+        </router-link>
+
+        <md-button>Action</md-button>
+      </md-card-actions>
+    </md-card>
+
         </div>
-        <div class="md-layout-item"></div>
-        <div class="md-layout-item"></div>
+        <div class="md-layout-item">
+            <md-card>
+      <md-card-header>
+        <div class="md-title">Card without hover effect</div>
+      </md-card-header>
+
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button>Action</md-button>
+        <md-button>Action</md-button>
+      </md-card-actions>
+    </md-card>
+
+        </div>
+        <div class="md-layout-item">
+            <md-card>
+      <md-card-header>
+        <div class="md-title">Card without hover effect</div>
+      </md-card-header>
+
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button>Action</md-button>
+        <md-button>Action</md-button>
+      </md-card-actions>
+    </md-card>
+        </div>
   </div>
 </template>
 
@@ -20,7 +68,7 @@ export default {
 </script>
 
 <style scoped>
-
+    
 </style>
 
 
