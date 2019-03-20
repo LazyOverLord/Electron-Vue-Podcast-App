e128f47a280a4bf153b119fbcaf1de2b82d77580
Sutton <suttonpage2000@gmail.com>
2019-03-20 16:43:17 +0300

    Added else statement to handle case where feed length is not divisable by 50 without a remainder

--- src/renderer/store/modules/data_store.js
+++ src/renderer/store/modules/data_store.js
@@ -93,12 +93,18 @@ const mutations = {
         var check_range = limted_data_length + 50;
         
 
-        check_range <= feed_length;
+        
         if(check_range <= feed_length){
             var new_data = state.podcast_feed_data[i].data.slice(limted_data_length,check_range);
             for(var t = 0;t<new_data.length;t++){
               state.podcast_feed_data[i].limited_data.push(new_data[t]);
             }
+        }
+        else{
+          var new_data = state.podcast_feed_data[i].data.slice(limted_data_length,feed_length);
+          for(var t = 0;t<new_data.length;t++){
+            state.podcast_feed_data[i].limited_data.push(new_data[t]);
+          }
         }
 
         }
