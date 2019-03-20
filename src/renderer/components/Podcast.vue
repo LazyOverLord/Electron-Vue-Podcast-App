d55bcf38cd730646749c845b94f8aafc09cb3eea
Sutton <suttonpage2000@gmail.com>
2019-03-20 17:16:21 +0300

    Added code to remove html tags from podcast episode descriptions

--- src/renderer/components/Podcast.vue
+++ src/renderer/components/Podcast.vue
@@ -109,8 +109,16 @@ export default {
                     var desc = items[i].getElementsByTagName("description")[0].textContent;
                     var enclosure = items[i].getElementsByTagName("enclosure")[0];
                     var url = enclosure.getAttribute("url");
+
+                    var temp_desc = "<div>" + desc +"</div>";
+                    var parser = new DOMParser();
+                    var desc_parser = parser.parseFromString(temp_desc,"text/html");
+                    var main_div = desc_parser.getElementsByTagName("div")[0];
+                    var clean_desc = main_div.innerText;
+
+
                     temp["title"] = title;
-                    temp["desc"] = desc;
+                    temp["desc"] = clean_desc;
                     temp["url"] = url;
                     episodes.push(temp);
                     }
